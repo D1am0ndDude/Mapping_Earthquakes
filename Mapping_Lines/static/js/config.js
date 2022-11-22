5c90c0f753df1fa37d0b3d1446ead458ebf31f48
Logan Knutson <46943357+D1am0ndDude@users.noreply.github.com>
2022-11-22 15:50:18 -0500

    Add files via upload

---
+++ Mapping_Lines/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoibG9nYW4ta251dHNvbiIsImEiOiJjbGFyY2NsYjYxcXNmM3FsZXdlbjhmbXpxIn0.efX2vA2i-0M8WMLJAEuiZQ";
---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,33 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6214, -122.3790], 5);
+
+// Coordinates for each point to be used in the polyline.
+let line = [
+  
+  [37.6214, -122.3790],
+  [40.63972, -73.77889],
+  [43.676670, -79.630550],
+  [30.202301, -97.669739]
+  
+];
+// Create a polyline using the line coordinates and make the line yellow.
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
